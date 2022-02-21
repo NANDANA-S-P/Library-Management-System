42699f0291487f9966b24afc8c8085e33d87e179
Nandana <nandana.20cs@kct.ac.in>
2022-02-21 19:28:19 +0530

    Added crud operations

--- lib_management/dashboard/models.py
+++ lib_management/dashboard/models.py
@@ -3,13 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    bookid = models.CharField(verbose_name="Book id", primary_key=True, max_length=6)
+    bookid = models.CharField(verbose_name="Book id",
+                              primary_key=True, max_length=6)
     title = models.TextField(verbose_name="Title", max_length=255)
     price = models.IntegerField(verbose_name="Price")
     desc = models.TextField(verbose_name="Description")
     publication = models.TextField(verbose_name="Publication")
     isbn = models.CharField(verbose_name="ISBN", max_length=20)
-    picture = models.ImageField(verbose_name="Picture", upload_to='books', blank=True)
+    picture = models.ImageField(
+        verbose_name="Picture", upload_to='books', blank=True)
 
     def __str__(self):
         return self.bookid + " " + self.title
@@ -23,7 +25,8 @@ class Copy(models.Model):
     ]
     bookid = models.ForeignKey(Book, on_delete=models.CASCADE)
     copy_no = models.IntegerField(verbose_name="Copy no")
-    status = models.TextField(verbose_name="status", choices=status_choices, default='Available')
+    status = models.TextField(verbose_name="status",
+                              choices=status_choices, default='Available')
 
     def __str__(self):
         return str(self.bookid.bookid) + "/" + str(self.copy_no)
@@ -33,10 +36,13 @@ class Issue(models.Model):
     bookid = models.ForeignKey(Book, on_delete=models.CASCADE)
     roll_no = models.CharField(verbose_name="roll_no", max_length=8)
     copy_no = models.IntegerField(verbose_name="Copy no")
-    date_issued = models.DateField(verbose_name="Date Issued", auto_now_add=True)
-    date_returned = models.DateField(verbose_name="Date Returned", blank=True)
+    date_issued = models.DateField(
+        verbose_name="Date Issued", auto_now_add=True)
+    date_returned = models.DateField(
+        verbose_name="Date Returned", blank=True, null=True)
     renewed = models.BooleanField(verbose_name="Renewed", default=False)
-    date_renewed = models.DateField(verbose_name="Date Renewed", blank=True)
+    date_renewed = models.DateField(
+        verbose_name="Date Renewed", blank=True, null=True)
 
 
 class Author(models.Model):
@@ -53,7 +59,3 @@ class Damage(models.Model):
     copy_no = models.IntegerField(verbose_name="Copy no")
     damaged_on = models.DateField(verbose_name="Damaged on", auto_now_add=True)
     damage_desc = models.TextField(verbose_name="Damage Description")
-
-
-
-
--- lib_management/dashboard/views.py
+++ lib_management/dashboard/views.py
@@ -1,11 +1,39 @@
+from asyncio.windows_events import NULL
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from home.models import Student
 from .models import *
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
 
 
 # Create your views here.
 def dashboard(request):
+
+    if request.method == 'POST':
+        roll_no = request.POST['rollno']
+        idofbook = request.POST['bookid']
+        b = Book.objects.get(bookid=idofbook)
+        copy_no = request.POST['copyno']
+        issu = Issue.objects.create(
+            bookid=b, roll_no=roll_no, copy_no=copy_no, renewed=0)
+        print(issu)
+
+        issu.save()
+
+    tobereturned = Issue.objects.all()
+    d = []
+    for i in tobereturned:
+        print("1..", i.date_issued)
+        enddate = i.date_issued + timedelta(days=2)
+        print("2..", enddate)
+        if(enddate == date.today()):
+            print(i.date_issued, enddate, "yessss")
+
+        else:
+            print(i.date_issued, enddate, "nooooo")
+
     if request.session.get('roll_no'):
         student = Student.objects.get(roll_no=request.session['roll_no'])
         if student.is_admin:
@@ -22,6 +50,20 @@ def log_out(request):
 
 
 def renew(request):
+    if request.method == "POST":
+        rollno = request.POST['rollno']
+        bookid = request.POST['bookid']
+        copyno = request.POST['copyno']
+        toberenewed = Issue.objects.filter(bookid=bookid).filter(
+            roll_no=rollno).filter(copy_no=copyno)
+        for i in toberenewed:
+            print(i.bookid, i.roll_no, i.copy_no, i.renewed)
+            if i.renewed == False:
+
+                i.renewed = True
+            else:
+                print("cannot be renrewd")
+        toberenewed.save()
     if request.session.get('roll_no'):
         student = Student.objects.get(roll_no=request.session['roll_no'])
         return render(request, 'dashboard/renew.html', {'student': student})
@@ -92,8 +134,12 @@ def add_book(request):
 def view_damage_report(request):
     if request.session.get('roll_no'):
         student = Student.objects.get(roll_no=request.session['roll_no'])
+        detail = Damage.objects.all()
+        for i in detail:
+            print(i.bookid, i.roll_no, i.copy_no, i.damaged_on, i.damage_desc)
         if student.is_admin:
-            return render(request, 'dashboard/view_damage_report.html', {'student': student})
+
+            return render(request, 'dashboard/view_damage_report.html', {'student': student, 'detail': detail})
         else:
             return redirect('/me')
 
@@ -102,11 +148,23 @@ def view_damage_report(request):
 
 
 def report_damage(request):
+    if request.method == "POST":
+        roll_no = request.POST['rollno']
+        idofbook = request.POST['bookid']
+        b = Book.objects.get(bookid=idofbook)
+        copy_no = request.POST['copyno']
+        damagedesc = request.POST['damagedescription']
+        d = Damage.objects.create(
+            bookid=b, roll_no=roll_no, copy_no=copy_no, damage_desc=damagedesc)
+
+        d.save()
+
     if request.session.get('roll_no'):
         student = Student.objects.get(roll_no=request.session['roll_no'])
-        return render(request, 'dashboard/report_damage.html', {'student':student})
+        return render(request, 'dashboard/report_damage.html', {'student': student})
     else:
         return redirect('/me')
+
 
 def error404(request, exception):
     return render(request, 'dashboard/404.html')
